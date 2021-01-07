879cef321fe92f19770abe58fcf4195eafe196db
makio_mizutani <makio.mizutani0710@gamil.com>
2021-01-07 08:47:12 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  
+
+  get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/help'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  
+  get 'favorites/index'
+  post 'favorites', to: 'favorites#create'
+end
+
